922fadcfe5afb20dad982192b97d67b8456f9488
Tesla7D <grikzyrev@gmail.com>
2015-04-19 23:01:24 -0400

    MY OWN TEST FILE IS COOL NOW

--- test.php
+++ test.php
@@ -10,39 +10,271 @@ class Database{
 		}
 	}
 	
-	public function AddFullTime($input){
-		$query = $this->db->prepare('SELECT id FROM person WHERE
-		firstName = ? AND lastName = ? AND SIN = ? AND dateOfBirth = ? 
-		LIMIT 1');
+	private function AddEmployee($input, $status, $user_id){
 		$firstName = $input->firstName;
 		$lastName = $input->lastName;
 		$sin = $input->sin;
 		$dateOfBirth = $input->dateOfBirth;
+		$company = $input->company;
 		
+		//
+		//	Look for Companies
+		//
+		$query = $this->db->prepare('SELECT id FROM company WHERE
+		companyName = ?	LIMIT 1');
+		if(!$query) throw new Exception($this->db->error);
+		if(!$query->bind_param("s",$company)) throw new Exception($this->db->error);
+		if(!$query->execute()) throw new Exception($this->db->error);
+		if(!$query->store_result()) throw new Exception($this->db->error);
+	
+		if($query->num_rows == 0){		
+			echo "Company is not there<br>";
+			
+			$sql = "INSERT INTO Company (id, companyName)
+			VALUES (" . $user_id . ",'" . $company . "')";
+			
+			if ($this->db->query($sql) == FALSE){
+				throw new Exception("Error: " . $sql . "<br>" . mysqli_error($this->db));
+			}else{
+				echo "	Company added<br>";
+			}
+		}
+		
+		$query = $this->db->prepare('SELECT id FROM company WHERE
+		companyName = ?	LIMIT 1');
+		if(!$query) throw new Exception($this->db->error);
+		if(!$query->bind_param("s",$company)) throw new Exception($this->db->error);
+		if(!$query->execute()) throw new Exception($this->db->error);
+		if(!$query->store_result()) throw new Exception($this->db->error);
+		if($query->num_rows == 1){		
+			echo "Found a company<br>";
+			
+			if(!$query->bind_result($company_id)) throw new Exception($this->db->error);
+			$query->fetch();
+		}else{
+			throw new Exception("Database error<br>");
+		}
+	
+		//
+		//	Look for Persons
+		//
+		$query = $this->db->prepare('SELECT id FROM person WHERE
+		firstName = ? AND lastName = ? AND SIN = ? AND dateOfBirth = ? 
+		LIMIT 1');
 		if(!$query) throw new Exception($this->db->error);
 		if(!$query->bind_param("ssss",$firstName,$lastName,$sin,$dateOfBirth)) throw new Exception($this->db->error);
 		if(!$query->execute()) throw new Exception($this->db->error);
 		if(!$query->store_result()) throw new Exception($this->db->error);
 		
-		if($query->num_rows == 1){
-			if(!$query->bind_result($person_id)) throw new Exception($this->db->error);
-			$query->fetch();
+		if($query->num_rows == 0){		
+			echo "Person is not there<br>";
 			
-			$sql = "INSERT INTO Employee (id, company_id, person_id, workStatus, reasonForLeaving)
-			VALUES (" . 120 . "," . 2 . "," . $person_id . ",'" . "inactive" . "','" . "swag" . "')";
-			
-			if ($this->db->query($sql) == FALSE){
-				echo "Error: " . $sql . "<br>" . mysqli_error($this->db);
-			}			
-			echo "already exists";
-		}else{
 			$sql = "INSERT INTO Person (id, firstName, lastName, SIN, dateOfBirth)
-			VALUES (" . 1 . ",'" . $firstName . "','" . $lastName . "','" . $sin . "','" . $dateOfBirth . "')";
+			VALUES (" . $user_id . ",'" . $firstName . "','" . $lastName . "','" . $sin . "','" . $dateOfBirth . "')";
 			
 			if ($this->db->query($sql) == FALSE){
-				echo "Error: " . $sql . "<br>" . mysqli_error($this->db);
+				throw new Exception("Error: " . $sql . "<br>" . mysqli_error($this->db));
+			}else{
+				echo "	Person added<br>";
 			}
 		}
+		
+		$query = $this->db->prepare('SELECT id FROM person WHERE
+		firstName = ? AND lastName = ? AND SIN = ? AND dateOfBirth = ? 
+		LIMIT 1');
+		if(!$query) throw new Exception($this->db->error);
+		if(!$query->bind_param("ssss",$firstName,$lastName,$sin,$dateOfBirth)) throw new Exception($this->db->error);
+		if(!$query->execute()) throw new Exception($this->db->error);
+		if(!$query->store_result()) throw new Exception($this->db->error);
+		if($query->num_rows == 1){		
+			echo "Found a Person<br>";
+			
+			if(!$query->bind_result($person_id)) throw new Exception($this->db->error);
+			$query->fetch();
+		}else{
+			throw new Exception("Database error");
+		}
+		
+		//
+		//	Look for Employee
+		//
+		$query = $this->db->prepare('SELECT id FROM employee WHERE
+		company_id = ? AND person_id = ? LIMIT 1');
+		if(!$query) throw new Exception($this->db->error);
+		if(!$query->bind_param("ii",$company_id, $person_id)) throw new Exception($this->db->error);
+		if(!$query->execute()) throw new Exception($this->db->error);
+		if(!$query->store_result()) throw new Exception($this->db->error);
+		
+		if($query->num_rows == 0){		
+			echo "Employee is not there<br>";
+			
+			$sql = "INSERT INTO Employee (id, company_id, person_id, workStatus, reasonForLeaving)
+			VALUES (" . $user_id . "," . $company_id . "," . $person_id . ",'" . $status . "','" . "" . "')";
+			
+			if ($this->db->query($sql) == FALSE){
+				throw new Exception("Error: " . $sql . "<br>" . mysqli_error($this->db));
+			}else{
+				echo "	Employee added<br>";
+			}
+		}
+		
+		$query = $this->db->prepare('SELECT id FROM employee WHERE
+		company_id = ? AND person_id = ? LIMIT 1');
+		if(!$query) throw new Exception($this->db->error);
+		if(!$query->bind_param("ii",$company_id, $person_id)) throw new Exception($this->db->error);
+		if(!$query->execute()) throw new Exception($this->db->error);
+		if(!$query->store_result()) throw new Exception($this->db->error);
+		if($query->num_rows == 1){		
+			echo "Found a Employee<br>";
+			
+			if(!$query->bind_result($employee_id)) throw new Exception($this->db->error);
+			$query->fetch();
+		}else{
+			throw new Exception("Database error");
+		}
+		
+		return $employee_id;
+	}
+	
+	public function AddFullTime($input, $status, $user_id){
+		$employee_id = $this->AddEmployee($input, $status, $user_id);
+		$dateOfHire = $input->dateOfHire;
+		$dateOfTermination = $input->dateOfTermination;
+		$salary = $input->salary;
+		
+		//
+		//	Look for Full Time Employee
+		//
+		$query = $this->db->prepare('SELECT id FROM fulltimeemployee WHERE
+		employee_id = ? AND dateOfHire = ? AND dateOfTermination = ? AND salary = ? LIMIT 1');
+		if(!$query) throw new Exception($this->db->error);
+		if(!$query->bind_param("issd",$employee_id, $dateOfHire, $dateOfTermination, $salary)) throw new Exception($this->db->error);
+		if(!$query->execute()) throw new Exception($this->db->error);
+		if(!$query->store_result()) throw new Exception($this->db->error);
+		
+		if($query->num_rows == 0){		
+			echo "FullTimeEmployee is not there<br>" ;
+			
+			$sql = "INSERT INTO FullTimeEmployee (id, employee_id, dateOfHire, dateOfTermination, salary)
+			VALUES (" . $user_id . "," . $employee_id . ",'" . $dateOfHire . "','" . $dateOfTermination . "','" . $salary . "')";
+			
+			if ($this->db->query($sql) == FALSE){
+				throw new Exception("Error: " . $sql . "<br>" . mysqli_error($this->db));
+			}else{
+				echo "	FullTimeEmployee added<br>";
+			}
+		}
+		
+		$query = $this->db->prepare('SELECT id FROM fulltimeemployee WHERE
+		employee_id = ? AND dateOfHire = ? AND dateOfTermination = ? AND salary = ? LIMIT 1');
+		if(!$query) throw new Exception($this->db->error);
+		if(!$query->bind_param("issd",$employee_id, $dateOfHire, $dateOfTermination, $salary)) throw new Exception($this->db->error);
+		if(!$query->execute()) throw new Exception($this->db->error);
+		if(!$query->store_result()) throw new Exception($this->db->error);
+		if($query->num_rows == 1){		
+			echo "Found a FullTimeEmployee<br>";
+			
+			if(!$query->bind_result($fulltimeemployee_id)) throw new Exception($this->db->error);
+			$query->fetch();
+		}else{
+			throw new Exception("Database error");
+		}
+		
+		return $fulltimeemployee_id;
+	}
+	
+	public function AddPartTime($input, $status, $user_id){
+		$employee_id = $this->AddEmployee($input, $status, $user_id);
+		$dateOfHire = $input->dateOfHire;
+		$dateOfTermination = $input->dateOfTermination;
+		$hourlyRate = $input->hourlyRate;
+		
+		//
+		//	Look for Part Time Employee
+		//
+		$query = $this->db->prepare('SELECT id FROM parttimeemployee WHERE
+		employee_id = ? AND dateOfHire = ? AND dateOfTermination = ? AND hourlyRate = ? LIMIT 1');
+		if(!$query) throw new Exception($this->db->error);
+		if(!$query->bind_param("issd",$employee_id, $dateOfHire, $dateOfTermination, $hourlyRate)) throw new Exception($this->db->error);
+		if(!$query->execute()) throw new Exception($this->db->error);
+		if(!$query->store_result()) throw new Exception($this->db->error);
+		
+		if($query->num_rows == 0){		
+			echo "PartTimeEmployee is not there<br>" ;
+			
+			$sql = "INSERT INTO PartTimeEmployee (id, employee_id, dateOfHire, dateOfTermination, hourlyRate)
+			VALUES (" . $user_id . "," . $employee_id . ",'" . $dateOfHire . "','" . $dateOfTermination . "','" . $hourlyRate . "')";
+			
+			if ($this->db->query($sql) == FALSE){
+				throw new Exception("Error: " . $sql . "<br>" . mysqli_error($this->db));
+			}else{
+				echo "	PartTimeEmployee added<br>";
+			}
+		}
+		
+		$query = $this->db->prepare('SELECT id FROM parttimeemployee WHERE
+		employee_id = ? AND dateOfHire = ? AND dateOfTermination = ? AND hourlyRate = ? LIMIT 1');
+		if(!$query) throw new Exception($this->db->error);
+		if(!$query->bind_param("issd",$employee_id, $dateOfHire, $dateOfTermination, $hourlyRate)) throw new Exception($this->db->error);
+		if(!$query->execute()) throw new Exception($this->db->error);
+		if(!$query->store_result()) throw new Exception($this->db->error);
+		if($query->num_rows == 1){		
+			echo "Found a PartTimeEmployee<br>";
+			
+			if(!$query->bind_result($parttimeemployee_id)) throw new Exception($this->db->error);
+			$query->fetch();
+		}else{
+			throw new Exception("Database error");
+		}
+		
+		return $$parttimeemployee_id;
+	}
+	
+	public function AddSeasonal($input, $status, $user_id){
+		$employee_id = $this->AddEmployee($input, $status, $user_id);
+		$piecePay = $input->piecePay;
+		$season = $input->season;
+		$seasonYear = $input->seasonYear;
+		
+		//
+		//	Look for Seasonal Employee
+		//
+		$query = $this->db->prepare('SELECT id FROM seasonalemployee WHERE
+		employee_id = ? AND piecePay = ? AND season = ? AND seasonYear = ? LIMIT 1');
+		if(!$query) throw new Exception($this->db->error);
+		if(!$query->bind_param("idsi",$employee_id, $piecePay, $season, $seasonYear)) throw new Exception($this->db->error);
+		if(!$query->execute()) throw new Exception($this->db->error);
+		if(!$query->store_result()) throw new Exception($this->db->error);
+		
+		if($query->num_rows == 0){		
+			echo "SeasonalEmployee is not there<br>" ;
+			
+			$sql = "INSERT INTO SeasonalEmployee (id, employee_id, piecePay, season, seasonYear)
+			VALUES (" . $user_id . "," . $employee_id . "," . $piecePay . ",'" . $season . "'," . $seasonYear . ")";
+			
+			if ($this->db->query($sql) == FALSE){
+				throw new Exception("Error: " . $sql . "<br>" . mysqli_error($this->db));
+			}else{
+				echo "	SeasonalEmployee added<br>";
+			}
+		}
+		
+		$query = $this->db->prepare('SELECT id FROM seasonalemployee WHERE
+		employee_id = ? AND piecePay = ? AND season = ? AND seasonYear = ? LIMIT 1');
+		if(!$query) throw new Exception($this->db->error);
+		if(!$query->bind_param("idsi",$employee_id, $piecePay, $season, $seasonYear)) throw new Exception($this->db->error);
+		if(!$query->execute()) throw new Exception($this->db->error);
+		if(!$query->store_result()) throw new Exception($this->db->error);
+		if($query->num_rows == 1){		
+			echo "Found a SeasonalEmployee<br>";
+			
+			if(!$query->bind_result($seasonalemployee_id)) throw new Exception($this->db->error);
+			$query->fetch();
+		}else{
+			throw new Exception("Database error");
+		}
+		
+		return $seasonalemployee_id;
 	}
 }
 
@@ -51,6 +283,7 @@ abstract class EmployeeModel{
 	protected $lastName;
 	protected $dateOfBirth;
 	protected $sin;
+	protected $company;
 	
 	public function SetFirstName($firstName){
 		// todo: validation logic
@@ -84,6 +317,13 @@ abstract class EmployeeModel{
 		return true;		
 	}
 	
+	public function SetCompany($company){
+		// todo: validation logic
+		// don't set class variable if invalid
+		// return false if invalid
+		$this->company = $company;
+		return true;		
+	}
 }
 
 abstract class HourlyEmployeeModel extends EmployeeModel{
@@ -137,12 +377,66 @@ class FullTimeEmployeeModel extends HourlyEmployeeModel{
 	}
 }
 
+class PartTimeEmployeeModel extends HourlyEmployeeModel{
+	private $hourlyRate;
+	
+	public function SetHourlyRate($hourlyRate){
+		// todo: validation logic
+		// don't set class variable if invalid
+		// return false if invalid
+		$this->hourlyRate = $hourlyRate;
+		return true;
+	}
+	
+	public function __get($key){
+		return $this->$key;
+	}
+}
+
+class SeasonalEmployeeModel extends EmployeeModel{
+	private $piecePay;
+	private $season;
+	private $seasonYear;
+
+	public function SetPiecePay($piecePay){
+		// todo: validation logic
+		// don't set class variable if invalid
+		// return false if invalid
+		$this->piecePay = $piecePay;
+		return true;
+	}
+		
+	public function SetSeason($season){
+		// todo: validation logic
+		// don't set class variable if invalid
+		// return false if invalid
+		$this->season = $season;
+		return true;
+	}
+	
+	public function SetSeasonYear($seasonYear){
+		// todo: validation logic
+		// don't set class variable if invalid
+		// return false if invalid
+		$this->seasonYear = $seasonYear;
+		return true;
+	}
+	
+	public function __get($key){
+		return $this->$key;
+	}
+}
+
 $obj = new FullTimeEmployeeModel;
 $db = new Database;
 
-$obj->SetFirstName("Greg");
+$obj->SetFirstName("Grogg");
 $obj->SetLastName("Koz");
 $obj->SetSIN("1234567890");
 $obj->SetDateOfBirth("1996/01/22");
-$db->AddFullTime($obj);
+$obj->SetCompany("Google");
+$obj->SetDateOfHire("2015/5/1");
+$obj->SetDateOfTermination("2015/8/31");
+$obj->SetSalary(10000);
+$db->AddFullTime($obj, "inactive", 1);
 ?>
