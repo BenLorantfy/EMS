8e998a2a506b07e8a87bfbab9d8b42e284be6d7c
Ben <ben@Bens-MacBook-Pro-2.local>
2015-04-20 01:16:05 -0400

    startzerz

--- php/Controllers/EmployeeController.php
+++ php/Controllers/EmployeeController.php
@@ -18,6 +18,22 @@ class EmployeeController{
 	}
 	
 	public function createEmployee($request){
+		switch($request->type){
+			case "fulltime":
+				$employee = new FullTimeEmployeeModel();
+				$employee->SetFirstName($request->firstName);
+				$employee->SetLastName($request->lastName);
+				$employee->SetDateOfBirth($request->dateOfBirth);
+				$employee->SetSIN($request->sin);
+				return $employee;
+			break;
+			case "parttime":
+			break;
+			case "seasonal":
+			break;
+			case "contract":
+			break;
+		}
 		return $request->type;
 	}
 }
