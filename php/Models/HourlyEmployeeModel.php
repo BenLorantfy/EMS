6d167b9a116b25eddd5ef15aa57ad8553ec6c7a8
Kevinljh <Kevinljh@foxmail.com>
2015-04-24 17:58:25 -0400

    Update HourlyEmployeeModel.php

--- php/Models/HourlyEmployeeModel.php
+++ php/Models/HourlyEmployeeModel.php
@@ -1,3 +1,10 @@
+//FILE			: HourlyEmployeeModel.php
+//PROJECT		: INFO2030-15W - Software Quality II - Final Project EMS
+//PROGRAMMER	: Dev Til Death: Grigoriy Kozyrev, Ben Lorantfy, Michael L. Da Silva, Kevin Li
+//FIRST VERSION	: 4/04/2015
+//DESCRIPTION	: This file contains the server side validation before the data goes into database, and asign the 
+//		  values for hourly employee. 
+
 <?php
 namespace Models;
 abstract class HourlyEmployeeModel extends EmployeeModel{
