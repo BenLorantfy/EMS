ab8b15f5bf7a21540f0c1ea34bc55bf973ece5f4
Kevinljh <Kevinljh@foxmail.com>
2015-04-24 18:23:04 -0400

    Update PartTimeEmployeeModel.php

--- php/Models/PartTimeEmployeeModel.php
+++ php/Models/PartTimeEmployeeModel.php
@@ -11,6 +11,12 @@ namespace Models;
 class PartTimeEmployeeModel extends HourlyEmployeeModel{
 	private $hourlyRate;
 	
+	//
+	// FUNCTION    : SetHourlyRate
+	// DESCRIPTION : set hourlyRate of employee
+	// PARAMETERS  : $hourlyRate
+	// RETURNS     : bool : the result of asign
+	//
 	public function SetHourlyRate($hourlyRate){
 		// todo: validation logic
 		// don't set class variable if invalid
